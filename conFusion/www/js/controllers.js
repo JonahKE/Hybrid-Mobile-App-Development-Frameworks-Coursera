eff4f80ed16bba0d1d6f7afee193455478d05e0a
Beñat Lizarazu <blizarazu@gmail.com>
2016-02-08 00:37:07 +0100

    Task 1

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -166,7 +166,7 @@ angular.module('conFusion.controllers', [])
     };
         }])
 
-.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL', function ($scope, $stateParams, menuFactory, baseURL) {
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicPopover', function ($scope, $stateParams, menuFactory, favoriteFactory, baseURL, $ionicPopover) {
 
     $scope.baseURL = baseURL;
     $scope.dish = {};
@@ -186,6 +186,23 @@ angular.module('conFusion.controllers', [])
             }
         );
 
+    $ionicPopover.fromTemplateUrl('templates/dish-detail-popover.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+
+    $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
 
         }])
 
